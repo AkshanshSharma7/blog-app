fdf65367607ed656802ec907f05e06ab9c385c73
AkshanshSharma7 <akshanshsharma56@gmail.com>
2024-07-15 17:19:58 +0530

    added vercel.json file

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -30,7 +30,7 @@ export default function LoginPage() {
   }
   return (
     <form className="login" onSubmit={login}>
-      <h1>Login</h1>
+      <h1 className="text-blue-800">Login</h1>
       <input type="text"
              placeholder="username"
              value={username}
